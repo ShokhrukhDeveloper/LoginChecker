ddb9b0bf0676ebbe138ef49db539abb6bd2f9cb7
Shoxrux <shoxruxyarashovuz@gmail.com>
2024-03-11 06:45:27 +0500

    Add project files.

---
+++ LoginChecker/Brokers/IStorageBroker.cs
@@ -0,0 +1,15 @@
+﻿using LoginChecker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginChecker.Brokers
+{
+    internal interface IStorageBroker
+    {
+        Credential[] GetAllCredentials();
+
+    }
+}
---
+++ LoginChecker/Brokers/StorageBoker.cs
@@ -0,0 +1,15 @@
+﻿using LoginChecker.Models;
+
+namespace LoginChecker.Brokers
+{
+    internal class StorageBoker : IStorageBroker
+    {
+        private static Credential[] credentials =
+        {
+            new Credential{UserName="shox", Password="12345" },
+            new Credential { UserName = "shox1", Password = "1233345" }
+        };
+
+        public Credential[] GetAllCredentials() => credentials;
+    }
+}
